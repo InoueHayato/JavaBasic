0c530085eb21097b151a9cc936e0ab0fd016f701
井上颯人 <inouesatsuhito@inouesatsuhitonoMacBook-Pro.local>
2019-02-22 15:26:11 +0900

    3-5

--- Practice/src/practice03/PTra03_05.java
+++ Practice/src/practice03/PTra03_05.java
@@ -34,5 +34,22 @@ public class PTra03_05 {
 		 * ※ プログラムは何行書いても良いです
 		 */
 
+
+		if(num >= 101) {
+			System.out.println("不正な値が入力されました");
+		}
+		else if(num <=0 ){
+			System.out.println("不正な値が入力されました");
+		}
+		else if (num >= 75) {
+			System.out.println("雨が降る確率が高いです");
+		}
+		else if(num >= 30 ){
+			System.out.println("雨が降るかもしれないです");
+		}
+		else if(num <= 29){
+			System.out.println("雨はまず降らないでしょう");
+		}
 	}
 }
+
