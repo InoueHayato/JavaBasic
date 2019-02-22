77d219db8ffb466aedd97153782bed4a027dc0b7
井上颯人 <inouesatsuhito@inouesatsuhitonoMacBook-Pro.local>
2019-02-22 13:04:32 +0900

    2-10

--- Practice/src/practice02/PTra02_01.java
+++ Practice/src/practice02/PTra02_01.java
@@ -10,19 +10,19 @@ public class PTra02_01 {
 	public static void main(String[] args) {
 
 		// ★ 20を5で割った値を出力してください
-        int sam;
-        sam=20/5;
-        System.out.println(sam);
+       int sam;
+       sam=20/5;
+       System.out.println(sam);
 
 		// ★ 126に13を掛けた値を出力してください
-        int sum;
-        sum=126*13;
-        System.out.println(sum);
+       int sim;
+       sim=126*13;
+       System.out.println(sim);
 
 		// ★ 97を6で割ったときの余りを出力してください
-        int sim;
-        sim=97%6;
-        System.out.println(sim);
+        int sum;
+        sum=97%6;
+        System.out.println(sum);
 
 	}
 }
